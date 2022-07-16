5df81d79be061c350d04bc6b769cbb74f19e4c5b
bosungpark <qkrqhtjd0806@naver.com>
2022-07-16 16:11:45 +0900

    Feat: initial commit

---
+++ src/main/java/com/onjung/onjung/OnjungApplication.java
@@ -0,0 +1,13 @@
+package com.onjung.onjung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnjungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnjungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onjung/onjung/OnjungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onjung.onjung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnjungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
