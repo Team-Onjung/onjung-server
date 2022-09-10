99266db39da4114338c9790774f99cd67ae956b1
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-09-10 15:34:20 +0900

    feat: 채팅 모듈 생성

---
+++ onjung-chatting/src/main/java/com/onjung/onjung/ChattingApplication.java
@@ -0,0 +1,12 @@
+package com.onjung.onjung;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class ChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingApplication.class, args);
+	}
+
+}
