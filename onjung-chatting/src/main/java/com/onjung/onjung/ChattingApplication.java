b274d6aa19aa4c3259e786f18e24219aaf803a19
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-09-10 15:43:35 +0900

    feat: 채팅 어플리케이션 패키지 생성

--- onjung-chatting/src/main/java/com/onjung/onjung/ChattingApplication.java
+++ onjung-chatting/src/main/java/com/onjung/onjung/ChattingApplication.java
@@ -2,7 +2,9 @@ package com.onjung.onjung;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ChattingApplication {
 
 	public static void main(String[] args) {
