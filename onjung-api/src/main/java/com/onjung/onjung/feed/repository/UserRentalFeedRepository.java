ea8b014de23a54f2c217879378a7b516faf664bf
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-09-11 01:47:38 +0900

    mypage 기능 구현

--- onjung-api/src/main/java/com/onjung/onjung/feed/repository/UserRentalFeedRepository.java
+++ onjung-api/src/main/java/com/onjung/onjung/feed/repository/UserRentalFeedRepository.java
@@ -1,12 +1,21 @@
 package com.onjung.onjung.feed.repository;
 
+import com.onjung.onjung.feed.domain.ClientFeed;
+import com.onjung.onjung.feed.domain.ServerFeed;
 import com.onjung.onjung.feed.domain.UserRentalFeed;
+import com.onjung.onjung.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRentalFeedRepository extends JpaRepository<UserRentalFeed,Long> {
-    Optional<UserRentalFeed> findById(Long id);
+
+//    유저와 관련된 모든 장부를 조회
+    @Query("select snapshot from UserRentalFeed snapshot where snapshot.clientUserID = :user or snapshot.serverUserID = :user")
+    List<Optional<UserRentalFeed>> findSnapshots(@Param("user") User user);
 }
--- onjung-api/src/main/java/com/onjung/onjung/user/dto/MypageResponseDto.java
+++ onjung-api/src/main/java/com/onjung/onjung/user/dto/MypageResponseDto.java
@@ -1,32 +1,37 @@
 package com.onjung.onjung.user.dto;
 
+import com.onjung.onjung.feed.domain.ClientFeed;
+import com.onjung.onjung.feed.domain.ServerFeed;
 import com.onjung.onjung.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Getter
 @Setter
-public class MypageResponseDto {
+public class MypageResponseDto implements Serializable {
 
-    String uuid;
-
-    String location_id;
-
-    String provider;
-
-    String profileImg;
-
-    String profileIntro;
-
-    String phone;
-
+//    닉네임 예: 얼리어답터 지우
     String username;
 
-    LocalDate birth;
+//    나의 상품
+    ArrayList<ServerFeed> myLentFeeds;
 
-    String university;
+//    대여한 상품
+    ArrayList<ServerFeed> myBorrowedFeeds;
+
+//    대여 요청
+    ArrayList<ClientFeed> myRequesteddFeeds;
+
+//    나의 정
+//    String myAffection; -> 이후 추가 예정
+
+//    찜 목록 -> 이후 추가 예정
+
+//    거래후기 -> 이후 추가 예정
 
 }
