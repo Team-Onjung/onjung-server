59e9c313e576227e0c4170c122e3b2b58677283a
Jin409 <sualng123@gmail.com>
2022-09-16 16:30:48 +0900

    chore: 채팅 관련 설정

--- onjung-chatting/src/main/java/com/onjung/onjung/common/config/WebSocketConfig.java
+++ onjung-chatting/src/main/java/com/onjung/onjung/common/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -18,6 +19,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // 해당 주소를 구독하고 있는 클라이언트들에 메시지 전달
         registry.enableSimpleBroker("/room");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws-stomp")
+                .withSockJS();
     }
 
 
