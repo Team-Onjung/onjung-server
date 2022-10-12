be9796984ebb8e64e4ab754aa44ab7b8f90cea16
Jin409 <sualng123@gmail.com>
2022-10-12 15:53:49 +0900

    refactor: 리뷰 컨트롤러 리팩토링

--- onjung-api/src/main/java/com/onjung/onjung/review/controller/ReviewController.java
+++ onjung-api/src/main/java/com/onjung/onjung/review/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.onjung.onjung.review.controller;
 
+import com.onjung.onjung.common.auth.PrincipalDetails;
 import com.onjung.onjung.exception.DataNotFoundException;
 import com.onjung.onjung.exception.InvalidParameterException;
 import com.onjung.onjung.exception.UnauthorizedException;
@@ -12,6 +13,7 @@ import com.onjung.onjung.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
@@ -22,6 +24,7 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/review")
 public class ReviewController {
 
     private final UserRepository userRepository;
@@ -30,49 +33,24 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping("/review")
-    public ResponseEntity createReview(@RequestBody @Valid ReviewRequestDto requestDto, BindingResult result) throws Exception{
+    @PostMapping("")
+    public ResponseEntity createReview(@RequestBody @Valid ReviewRequestDto requestDto, @AuthenticationPrincipal PrincipalDetails principalDetails, BindingResult result) throws Exception{
 
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if ((String)principal!= "anonymousUser") {
-            Optional<User> _user = userRepository.findByUsername((String) principal);
-
-            if (_user.isPresent()) {
-                User user = _user.get();
-                reviewService.createReview(requestDto, user);
-
-                if (result.hasErrors()) {
-                    throw new InvalidParameterException(result);
-                }
-
-                return ResponseEntity.status(HttpStatus.OK).body("ok");
-            }
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("user not found");
-        }else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("you need to login");
-        }
+        User user = principalDetails.getUser();
+        reviewService.createReview(requestDto, user);
+        return ResponseEntity.status(HttpStatus.OK).body("리뷰가 등록되었습니다.");
     }
 
     @DeleteMapping("/review/{reviewId}")
-    public ResponseEntity deleteReview(@PathVariable("reviewId") Long reviewId) throws DataNotFoundException{
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ResponseEntity deleteReview(@PathVariable("reviewId") Long reviewId,
+                                       @AuthenticationPrincipal PrincipalDetails principalDetails) throws DataNotFoundException{
 
-        if ((String)principal!= "anonymousUser"){
-
-            Optional<User> user = userRepository.findByEmail((String) principal);
-            Optional<Review> findReview = reviewRepository.findById(reviewId);
-
-            if (user.isPresent() && user.get().equals(findReview.get().getSender())){
-                if (user.get().equals(findReview.get().getSender()))
-                    reviewService.deleteReview(reviewId);
-                return ResponseEntity.status(HttpStatus.OK).body("ok");
-            }else
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("you can not delete feed that you did not write");
+        User user = principalDetails.getUser();
+        Review findReview = reviewRepository.findById(reviewId).orElseThrow(DataNotFoundException::new);
+        if (user.equals(findReview.getSender())){
+            reviewService.deleteReview(reviewId);
         }
-        else
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("you can not delete feed that you did not write");
-
+        return ResponseEntity.status(HttpStatus.OK).body("리뷰가 삭제되었습니다.");
     }
 
 }
